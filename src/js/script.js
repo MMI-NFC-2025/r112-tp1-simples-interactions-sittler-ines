ecf51bd5f7f839f8856157d1fde4e2a09c90cf2e
sittler-ines <ines.sittler@edu.univ-fcomte.fr>
2025-11-04 15:30:21 +0100

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 // TODO
+const AMettreEnRouge=document.querySelector('#a-mettre-en-rouge');
+AMettreEnRouge.style.color='red';
+
+const enRougeSuiteAClick = document.querySelector('#en-rouge-suite-a-click');
+    enRougeSuiteAClick.addEventListener('click', (evt) => {
+        enRougeSuiteAClick.style.color = "red";
+    });
+
+const collH2 = document.querySelectorAll("h2");
+    elm.addEventListener("click", (evt) => {
+        evt.target.style.color = "red";
+    });
